f7a96eb81dd1f54e995de184ffa521132feee882
Saksham Solanki <68708945+saksham2105@users.noreply.github.com>
2021-10-09 20:42:48 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,13 +1,16 @@
+/**
+* A main Class to verify if all BoilerPlate has been generated successfully or not
+*/
 class Program
 {
 public static void main(String[] gg)
 {
 Employee emp = new Employee();
 emp.setId(101);
-emp.setFirstName("Kalu");
+emp.setFirstName("John");
 emp.setSalary(10000.0);
 emp.setGender('M');
-System.out.println(emp);
+System.out.println(emp); //To String will be invoked
 System.out.println(emp.getFirstName());
 System.out.println(emp.getId());
 System.out.println(emp.getSalary() instanceof Double);
